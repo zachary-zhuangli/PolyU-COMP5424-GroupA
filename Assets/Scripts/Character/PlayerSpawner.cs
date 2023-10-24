8d07c09e48b38dae524eace0043daeec2196d230
L0xfeee <949570766@qq.com>
2023-10-24 20:23:31 +0800

    feat: 动态设置初始人物高度

--- Assets/Scripts/Character/PlayerSpawner.cs
+++ Assets/Scripts/Character/PlayerSpawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
+using BNG;
 
 // 玩家角色Avatar 和 玩家XR设备位姿数据同步组件 的生成器
 // 本地和网络玩家角色Avatar的身体姿态 都将通过对应XR设备位姿数据在本地基于IK实时运算
@@ -9,7 +10,9 @@ using Fusion;
 public class PlayerSpawner : NetworkBehaviour
 {
     public NetworkPrefabRef PlayerPrefab = NetworkPrefabRef.Empty;
+    public float AvatarHeight = 1.55f;
     public NetworkPrefabRef XRPosPrefab = NetworkPrefabRef.Empty;
+    public GameObject BNGPlayerControllerGO;
 
     public NetworkObject localPlayer { get; private set; }
 
@@ -30,6 +33,7 @@ public class PlayerSpawner : NetworkBehaviour
         Runner.Spawn(XRPosPrefab, new Vector3(0, 0, 0), Quaternion.identity, Runner.LocalPlayer);
         this.localPlayer = Runner.Spawn(PlayerPrefab, new Vector3(-77, 121, -28), Quaternion.identity, Runner.LocalPlayer);
         HideLocalPlayer(this.localPlayer);
+        SetCharacterHeight();
     }
 
     private void HideLocalPlayer(NetworkObject playerGO)
@@ -39,6 +43,22 @@ public class PlayerSpawner : NetworkBehaviour
         foreach (Transform tran in playerGO.GetComponentsInChildren<Transform>())
         {
             tran.gameObject.layer = LayerMask.NameToLayer("Hide");
+        }
+    }
+
+    private void SetCharacterHeight()
+    {
+        CharacterController characterController = BNGPlayerControllerGO.GetComponent<CharacterController>();
+        BNGPlayerController bngPlayerController = BNGPlayerControllerGO.GetComponent<BNGPlayerController>();
+        if (characterController && bngPlayerController)
+        {
+            float currentHeight = characterController.height;
+            float offset = AvatarHeight - currentHeight;
+            bngPlayerController.CharacterControllerYOffset = offset;
+        }
+        else
+        {
+            Debug.LogError("CharacterController is null when setup CharacterHeight");
         }
     }
 }
